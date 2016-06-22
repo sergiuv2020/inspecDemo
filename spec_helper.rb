27fce4eb569564b64aa95ddb3cfdb1a33375157c
Sergiu Vidrascu <vsergiu@hotmail.com>
2016-06-22 11:09:15 +0300

    Update spec_helper.rb

--- spec_helper.rb
+++ spec_helper.rb
@@ -4,7 +4,7 @@ require_relative 'helpers'
 
 include Helpers
 
-# RSpec.configure do |c|
-#   c.output_stream = File.open("#{Dir.pwd}/inspec.xml", 'w')
-#   c.formatter = 'JUnit'
-# end
+RSpec.configure do |c|
+#  c.output_stream = File.open("#{Dir.pwd}/inspec.xml", 'w')
+  c.formatter = 'documentation'
+end
